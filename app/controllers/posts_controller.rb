8ababa0582708c51ab22d23403e09671a7449b32
Trevor Scherling <scherling.trevor@gmail.com>
2015-10-18 15:26:12 -0700

    set up posts routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,48 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    render :index
   end
 
   def show
     @post = List.find(params[:id])
-    render :show
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to post_path
+  end
+
+
+  private
+  def post_params
+    params.require(:list).permit(:title, :content)
+  end
+
 end
